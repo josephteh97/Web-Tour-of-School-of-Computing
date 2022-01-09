7aa10656fbf6c73f45b8259ee6007777d87f15be
josephteh97 <96156922+josephteh97@users.noreply.github.com>
2022-01-09 21:38:17 +0800

    Computing School Web Tour
    Web Tour

---
+++ data.js
@@ -0,0 +1,176 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-com1-building_edited",
+      "name": "COM1 Building_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.40427682086401795,
+        "pitch": -0.33560167404923646,
+        "fov": 1.2959776848255018
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.40427682086401795,
+          "pitch": -0.33560167404923646,
+          "rotation": 6.283185307179586,
+          "target": "2-lobby_edited"
+        },
+        {
+          "yaw": 0.40427682086401795,
+          "pitch": -0.33560167404923646,
+          "rotation": 0,
+          "target": "1-foyer-at-level-2_edited"
+        },
+        {
+          "yaw": 0.40427682086401795,
+          "pitch": -0.33560167404923646,
+          "rotation": 4.71238898038469,
+          "target": "2-lobby_edited"
+        },
+        {
+          "yaw": 0.40427682086401795,
+          "pitch": -0.33560167404923646,
+          "rotation": 4.71238898038469,
+          "target": "2-lobby_edited"
+        },
+        {
+          "yaw": 0.40427682086401795,
+          "pitch": -0.33560167404923646,
+          "rotation": 0,
+          "target": "1-foyer-at-level-2_edited"
+        },
+        {
+          "yaw": -0.0722876110911077,
+          "pitch": -0.41887733942839844,
+          "rotation": 0,
+          "target": "2-lobby_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.40427682086401795,
+          "pitch": -0.33560167404923646,
+          "title": "Welcome to School of Computing",
+          "text": "This is the school of computing. Welcome you on behalf of all departments from the school."
+        }
+      ]
+    },
+    {
+      "id": "1-foyer-at-level-2_edited",
+      "name": "Foyer at Level 2_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.2979561514811877,
+        "pitch": 0.048154791235328176,
+        "fov": 1.2959776848255018
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2979561514811877,
+          "pitch": 0.048154791235328176,
+          "rotation": 4.71238898038469,
+          "target": "0-com1-building_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.671645883351701,
+          "pitch": 0.12970574806180046,
+          "title": "Foyer",
+          "text": "Welcome to Foyer"
+        }
+      ]
+    },
+    {
+      "id": "2-lobby_edited",
+      "name": "Lobby_edited",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.0466745034991636,
+        "pitch": 0.1561228993009216,
+        "fov": 1.2959776848255018
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8710070600791866,
+          "pitch": 0.20886387914464777,
+          "rotation": 13.351768777756625,
+          "target": "1-foyer-at-level-2_edited"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.5951089926542235,
+          "pitch": 0.3426128824144339,
+          "title": "Lobby",
+          "text": "Welcome to the lobby"
+        }
+      ]
+    }
+  ],
+  "name": "Visit the School of Computing",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
